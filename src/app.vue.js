344435b34ab683ac77c35eebb91bd1a9f5f06c16
Timothy Cool <tcoolpg@gmail.com>
2017-11-07 16:00:19 -0800

    convert app template to component

--- src/app.vue.js
+++ src/app.vue.js
@@ -1,10 +1,32 @@
 import Vue from 'vue'
 
+Vue.component('app', {
+    template : `
+        <div id="app">
+            <nav class="nav has-shadow">
+                <div class="container">
+                    <a href="/">
+                        <img src="http://bit.ly/vue-img" alt="Vue SPA"/>
+                    </a>
+                </div>
+            </nav>
+            <section class="main-section section"></section>
+            <footer class="footer">
+                <div class="container">
+                    <div class="content has-text-centered">
+                        Follow us on the
+                        <a href="https://twitter.com/tcool86" target="_blank">
+                            Twitter app?
+                        </a>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    `
+})
+
 const app = new Vue({
-    data : {
-        hello : 'this is a string that is hot loaded :) '
-    },
-    template : '<div id="app"> {{hello}} </div>'
+    render : renderApp => renderApp('app')
 })
 
 export { app }
