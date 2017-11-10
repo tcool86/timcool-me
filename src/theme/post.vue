497a966e9fb76823cc846b21b6a5e300444d7409
Timothy Cool <tcoolpg@gmail.com>
2017-11-09 17:43:51 -0800

    use scoped style for footer

--- src/theme/post.vue
+++ src/theme/post.vue
@@ -14,3 +14,15 @@
         props : ['link']
     }
 </script>
+<style scoped>
+    .card {
+        padding-bottom : 40px;
+        height : 100%;
+    }
+    footer {
+        position : absolute;
+        width : 100%;
+        left : 0;
+        bottom : 0;
+    }
+</style>
