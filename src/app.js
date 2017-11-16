e45a85c970adf2019a8db56cc46aeda972ab3ffd
Timothy Cool <tcoolpg@gmail.com>
2017-11-15 20:06:28 -0800

    use Vuex for state management

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
+import store from './vuex/index.js'
 import AppLayout from './theme/layout.vue'
 import router from './router.js'
 
 const app = new Vue({
     router,
-    ...AppLayout
+    ...AppLayout,
+    store
 })
 
-export { app, router }
+export { app, router, store }
