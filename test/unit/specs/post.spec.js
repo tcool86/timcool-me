f025542a777b985f52379cfcdacbb92508834c02
Timothy Cool <tcoolpg@gmail.com>
2017-11-20 10:51:42 -0800

    add some more post unit tests assert with chai

--- test/unit/specs/post.spec.js
+++ test/unit/specs/post.spec.js
@@ -2,13 +2,27 @@ import Vue from 'vue'
 import Post from '../../../src/theme/post.vue'
 
 describe('post.vue', () => {
-    it('should render the link', () => {
+    const createComponent = () => {
         const PostConstructor = Vue.extend(Post)
         const comp = new PostConstructor({
             propsData : {
                 link : 'http://www.pluralsight.com'
             }
         }).$mount()
-        console.log(comp)
+        return comp
+    }
+    it('should render the link', () => {
+        const comp = createComponent()
+        var elementLink = comp.$el.querySelector('.card-footer-item').getAttribute('href')
+        expect(elementLink).to.equal('http://www.pluralsight.com')
+    })
+    it('should update element\'s href when property link changes', (done) => {
+        const comp = createComponent()
+        comp.link = 'http://timcool.me'
+        Vue.nextTick(() => {
+            var elementLink = comp.$el.querySelector('.card-footer-item').getAttribute('href')
+            expect(elementLink).to.equal('http://timcool.me')
+            done()
+        })
     })
 })
