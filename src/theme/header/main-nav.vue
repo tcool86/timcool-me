7912551472d357970ab334654ce419d80b75b36e
Timothy Cool <tcoolpg@gmail.com>
2017-12-06 18:09:15 -0800

    some cleanup to menu styles

--- src/theme/header/main-nav.vue
+++ src/theme/header/main-nav.vue
@@ -13,6 +13,54 @@
         .nav-wrapper {
             z-index: 1;
             transition: all 0.15s ease-out;
+            a {
+                text-decoration: none;
+            }
+            .navbar-item {
+                margin-right: 0;
+                line-height: 0.4em;
+                display: inline-block;
+                text-transform: uppercase;
+                color: white;
+                background-color: $backgroundColor;
+                border-bottom: 2px solid $borderColor;
+                border-top: 2px solid $borderColor;
+                border-left: 1px solid $borderColor;
+                border-radius: 0%;
+                transition: 0.5s;
+                padding: 1.5rem;
+                &:first-child {
+                    border-left: 2px solid $borderColor;
+                }
+                &:last-child {
+                    border-right: 2px solid $borderColor;
+                }
+                &:active {
+                    cursor: pointer;
+                    transition: 0.1s;
+                    background-color: $activeColor;
+                    color: black;
+                }
+                &.router-link-exact-active {
+                    line-height: 0;
+                    background-color: white;
+                    color: black;
+                }
+                /* Desktop Only */
+                @media (min-width: $tabletSize) {
+                    &:hover {
+                        cursor: pointer;
+                        background-color: $highlightColor;
+                        color: black;
+                    }
+                }
+                @media (max-width: $mobileSize) {
+                    background-color: $backgroundColorMobile;
+                    color: $textColorMobile;
+                    padding: 1rem;
+                    font-size: 8pt;
+                }
+            }
             /* Tablet */
             @media (max-width: $tabletSize) {
                 width: 542px;
@@ -22,58 +70,7 @@
             /* Mobile */
             @media (max-width: $mobileSize) {
                 width: 400px;
-                margin: auto;
                 text-align: center;
-            }
-            a {
-                text-decoration: none;
-            }
-            .navbar-item {
-                margin-right: 0;
-                line-height: 0.4em;
-                display: inline-block;
-                text-transform: uppercase;
-                &:first-child {
-                    border-left: 2px solid $borderColor;
-                }
-                &:last-child {
-                    border-right: 2px solid $borderColor;
-                }
-                color: white;
-                background-color: $backgroundColor;
-                border-bottom: 2px solid $borderColor;
-                border-top: 2px solid $borderColor;
-                border-left: 1px solid $borderColor;
-
-                border-radius: 0%;
-                transition: 0.5s;
-                padding: 1.5rem;
-                /* Desktop Only */
-                @media (min-width: $mobileSize) {
-                    &:hover {
-                        cursor: pointer;
-                        background-color: $highlightColor;
-                        color: black;
-                    }
-                }
-                &:active {
-                    cursor: pointer;
-                    transition: 0.1s;
-                    background-color: $activeColor;
-                    color: black;
-                }
-                &.router-link-exact-active {
-                    line-height: 0em;
-                    background-color: white;
-                    color: black;
-                }
-                @media (max-width: 580px) {
-                    background-color: $backgroundColorMobile;
-                    color: $textColorMobile;
-                    padding: 1em;
-                    height: auto;
-                    font-size: 8pt;
-                }
             }
         }
     }
