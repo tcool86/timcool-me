e6cf69679992cd29d4829e2ed19e30f0137c6fd4
Timothy Cool <tcoolpg@gmail.com>
2017-12-14 23:55:59 -0800

    setup project vue component

--- src/theme/portfolio.vue
+++ src/theme/portfolio.vue
@@ -15,27 +15,22 @@
         </div>
         <section id="project-section">
             <div class="project-container">
-                <article class="project-article" >
-                    <!-- <div class="project-title-wrapper">
-                        <span class="platforms">Platforms go here</span>
-                        <span class="project-icon">
-                            <img src="web/assets/placeholder_app_icon.png" />
-                        </span>
-                        <h2 class="project-title">{{project.title}}</h2>
-                        <span class="project-date right">Last Updated: {{project.last_updated | date : 'mediumDate'}} </span>
-                        <span class="project-date right">Project Started: {{project.start_date | date : 'mediumDate'}}</span>
-                    </div>
-                    <div class="project-description-wrapper">
-                        <p class="project-description">{{project.description}}</p>
-                    </div>
-                    <div class="project-image-wrapper">
-                        <img class="project-image" ng-src="{{project.image}}">
-                    </div> -->
-                </article>
+                <project></project>
+                <project></project>
+                <project></project>
             </div>
         </section>
     </div>
 </template>
+<script>
+    import project from './project.vue'
+    export default {
+        components : {
+            'project' : project
+        }
+    }
+</script>
+
 <style lang="scss">
     @import '../styles/style-vars.scss';
 
---
+++ src/theme/project.vue
@@ -0,0 +1,19 @@
+<template>
+    <article class="project-article" >
+        <!-- <div class="project-title-wrapper">
+            <span class="platforms">Platforms go here</span>
+            <span class="project-icon">
+                <img src="web/assets/placeholder_app_icon.png" />
+            </span>
+            <h2 class="project-title">{{project.title}}</h2>
+            <span class="project-date right">Last Updated: {{project.last_updated | date : 'mediumDate'}} </span>
+            <span class="project-date right">Project Started: {{project.start_date | date : 'mediumDate'}}</span>
+        </div>
+        <div class="project-description-wrapper">
+            <p class="project-description">{{project.description}}</p>
+        </div>
+        <div class="project-image-wrapper">
+            <img class="project-image" ng-src="{{project.image}}">
+        </div> -->
+    </article>
+</template>
