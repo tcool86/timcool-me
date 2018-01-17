604987005bc26963a707a4aceda543985cc30a84
Timothy Cool <tcoolpg@gmail.com>
2018-01-16 20:31:41 -0800

    build with deeplink fixes

--- dist/server/main.js
+++ dist/server/main.js
@@ -985,6 +985,7 @@ var closeableClasses = ['detail-modal-background', 'detail-view-wrapper'];
             this.showDates = true;
         },
         handleProjectOnClick: function handleProjectOnClick() {
+            this.handleDeeplinkOpen();
             this.showDetailBackground = true;
         },
 
@@ -992,19 +993,26 @@ var closeableClasses = ['detail-modal-background', 'detail-view-wrapper'];
             var targetClass = event.target.className;
             event.stopPropagation();
             if (closeableClasses.includes(targetClass)) {
-                var deeplink = this.$route.params.deeplink;
-                if (typeof deeplink !== 'undefined') {
-                    if (typeof window !== 'undefined') {
-                        window.location.assign(window.location.origin + '/portfolio');
-                    }
-                } else {
-                    this.showDetail = false;
+                this.handleDeeplinkClose();
+                this.showDetail = false;
+            }
+        },
+        handleDeeplinkOpen: function handleDeeplinkOpen() {
+            var url = window.location.origin + '/portfolio/' + this.deeplinkName();
+            window.history.pushState(null, null, url);
+        },
+        handleDeeplinkClose: function handleDeeplinkClose() {
+            var deeplink = this.$route.params.deeplink;
+            if (typeof deeplink !== 'undefined') {
+                if (typeof window !== 'undefined') {
+                    var url = window.location.origin + '/portfolio';
+                    window.history.pushState(null, null, url);
                 }
             }
         },
         deeplinkName: function deeplinkName() {
             var name = this.project.title;
-            return name.toLowerCase().replace(/\s/, '-');
+            return name.toLowerCase().replace(/\s/g, '-');
         }
     },
     created: function created() {
@@ -2238,7 +2246,7 @@ var Component = normalizeComponent(
 "use strict";
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__babel_loader_node_modules_vue_loader_lib_selector_type_script_index_0_project_vue__ = __webpack_require__(18);
 /* unused harmony namespace reexport */
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__node_modules_vue_loader_lib_template_compiler_index_id_data_v_75afc7f9_hasScoped_false_buble_transforms_node_modules_vue_loader_lib_selector_type_template_index_0_project_vue__ = __webpack_require__(84);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__node_modules_vue_loader_lib_template_compiler_index_id_data_v_f8f59dae_hasScoped_false_buble_transforms_node_modules_vue_loader_lib_selector_type_template_index_0_project_vue__ = __webpack_require__(84);
 function injectStyle (ssrContext) {
 var i
 ;(i=__webpack_require__(80),i.__inject__&&i.__inject__(ssrContext),i)
@@ -2259,7 +2267,7 @@ var __vue_scopeId__ = null
 var __vue_module_identifier__ = "cfe4caa0"
 var Component = normalizeComponent(
   __WEBPACK_IMPORTED_MODULE_0__babel_loader_node_modules_vue_loader_lib_selector_type_script_index_0_project_vue__["a" /* default */],
-  __WEBPACK_IMPORTED_MODULE_1__node_modules_vue_loader_lib_template_compiler_index_id_data_v_75afc7f9_hasScoped_false_buble_transforms_node_modules_vue_loader_lib_selector_type_template_index_0_project_vue__["a" /* default */],
+  __WEBPACK_IMPORTED_MODULE_1__node_modules_vue_loader_lib_template_compiler_index_id_data_v_f8f59dae_hasScoped_false_buble_transforms_node_modules_vue_loader_lib_selector_type_template_index_0_project_vue__["a" /* default */],
   __vue_template_functional__,
   __vue_styles__,
   __vue_scopeId__,
