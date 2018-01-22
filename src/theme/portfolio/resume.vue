08154765feb3e6f07cbfa0436ce4f704014f2d1b
Timothy Cool <tcoolpg@gmail.com>
2018-01-22 00:57:22 -0800

    work on resume rs blurb

--- src/theme/portfolio/resume.vue
+++ src/theme/portfolio/resume.vue
@@ -1,48 +1,9 @@
 <template>
     <section class="resume section">
         <div class="resume-wrapper container">
-            <article class="content" id="rewardstyle-section">
-                <figure class="employer">
-                    <img src="//res.cloudinary.com/hnairgdu6/image/upload/v1515911748/rs_ltk_byc28h.png"/>
-                    <figcaption>
-                        <h2>rewardStyle + LIKEtoKNOW.it</h2>
-                    </figcaption>
-                </figure>
-                <p>
-                    Built and maintained features inside the rewardStyle iOS mobile app (influencer app and utilized technologies from the consumer app LIKEtoKNOW.it)
-                    <ol>
-                        <li> Objective-C and Swift.</li>
-                        <li> CoreData for persisting favorited items within up to thousands of directories. </li>
-                        <li> MVVM + MVC + MVP architectures.</li>
-                        <li> CocoaPods for pod dependency management.</li>
-                        <li> Bitrise for continuous integration, pushing to a branch deploys a build on TestFlight.</li>
-                        <li> IGListKit for complex list views with multiple controllers.</li>
-                        <li> AFNetworking with a custom wrapper for network calls.</li>
-                        <li> Amplitude for analytics across most of the app.</li>
-                        <li> Purelayout for dynamic layouts programmatically, used interface builder for legacy views.</li>
-                        <li> Third party API integrations for sharing products to Facebook, Tumblr, Twitter, and Pinterest.</li>
-                    </ol>
-
-                    Implementation of new rewardStyle API's for mobile app consumption.
-                    <ol>
-                        <li> Python and PHP for legacy endpoints.</li>
-                        <li> Stoplight for mocking up web services.</li>
-                        <li> Paw for organizing and outlining the revised LTK API.</li>
-                    </ol>
-                    <figure class="products">
-                        <img src="//res.cloudinary.com/hnairgdu6/image/upload/v1515911749/rewardstyle-ltk_xaacwj.png"/>
-                        <figcaption>
-                            (right to left) rewardStyle mobile app, LTK consumer app + website
-                        </figcaption>
-                    </figure>
-                    Built part of the front end web experience for rewardStyle's Display product
-                    <ol>
-                        <li> Javascript with AngularJS for the application framework. </li>
-                        <li> SASS for extending and generating CSS.</li>
-                        <li> Gulp for the build system, minifying, and linting.</li>
-                    </ol>
-                </p>
-            </article>
+            <transition name="fade">
+                <rs-blurb></rs-blurb>
+            </transition>
             <article class="content" id="thismoment-section">
                 <figure class="employer">
                     <img src="//res.cloudinary.com/hnairgdu6/image/upload/v1515911748/tmlogo_rylvaw.png"/>
@@ -60,12 +21,14 @@
                         <li> Amazon S3 to store UGC uploads.</li>
                         <li> ImageMagick to manipulate the UGC image uploads.</li>
                     </ol>
-                    <figure class="products">
-                        <img src="//res.cloudinary.com/hnairgdu6/image/upload/v1515911749/thismoment-clients_prein5.png"/>
-                        <figcaption>
-                            a few examples of sites built with DEC + Content Cloud
-                        </figcaption>
-                    </figure>
+                </p>
+                <figure class="products">
+                    <img src="//res.cloudinary.com/hnairgdu6/image/upload/v1515911749/thismoment-clients_prein5.png"/>
+                    <figcaption>
+                        a few examples of sites built with DEC + Content Cloud
+                    </figcaption>
+                </figure>
+                <p>
                     Rewrite and refactor legacy code to make it easy to maintain and extend upon. Wrote and designed framework and modular components for front end SDK.
                     <ol>
                         <li> Lots of custom PHP and Javascript.</li>
@@ -74,12 +37,14 @@
                         <li> Multiple analytics and tracking systems to fit client needs.</li>
                         <li> LESS for CSS organization and minimization.</li>
                     </ol>
-                    <figure class="products">
-                        <img src="//res.cloudinary.com/hnairgdu6/image/upload/v1515911749/thismoment-content-cloud_muf8y6.png"/>
-                        <figcaption>
-                            Content Cloud
-                        </figcaption>
-                    </figure>
+                </p>
+                <figure class="products">
+                    <img src="//res.cloudinary.com/hnairgdu6/image/upload/v1515911749/thismoment-content-cloud_muf8y6.png"/>
+                    <figcaption>
+                        Content Cloud
+                    </figcaption>
+                </figure>
+                <p>
                     Designed and wrote documentation for software features and presented those in a biweekly review.
                 </p>
             </article>
@@ -99,38 +64,107 @@
                         <li> Implemented Prestashop cart and developed custom module for preorders. </li>
                         <li> HTML/CSS/JS with jQuery and AJAX techniques for web site and administrative site frontend.</li>
                     </ol>
-                    <figure class="products">
-                        <img src="//res.cloudinary.com/hnairgdu6/image/upload/v1515911749/hf-backend-tools_jnvctl.png"/>
-                        <img src="//res.cloudinary.com/hnairgdu6/image/upload/v1515911751/hf-frontend_toveca.png"/>
-                        <figcaption>
-                            Admin tools and the frontend site
-                        </figcaption>
-                    </figure>
+                </p>
+                <figure class="products">
+                    <img src="//res.cloudinary.com/hnairgdu6/image/upload/v1515911749/hf-backend-tools_jnvctl.png"/>
+                    <img src="//res.cloudinary.com/hnairgdu6/image/upload/v1515911751/hf-frontend_toveca.png"/>
+                    <figcaption>
+                        Admin tools and the frontend site
+                    </figcaption>
+                </figure>
+                <p>
                     Worked in a small team and was responsible for a variety of functions including processing orders and organizing shipments for conventions.
                 </p>
             </article>
         </div>
     </section>
 </template>
+<script>
+    import rewardStyleBlurb from './rs-blurb.vue'
+
+    export default {
+        components : {
+            'rs-blurb' : rewardStyleBlurb
+        }
+    }
+</script>
 <style lang="scss">
+    @import '../../styles/style-vars.scss';
     .resume-wrapper {
         color: silver;
         article {
-            padding-bottom: 1rem;
-            margin-bottom: 1rem;
+            content {
+                margin-bottom: 4rem;
+            }
+            padding-bottom: 4rem;
             border-bottom: 0.1rem solid darkgrey;
+            p {
+                background-color: $paragraphBackground;
+                border-radius: 1rem;
+                padding: 1rem;
+                margin: 1rem;
+            }
         }
-        h2 {
-            color: whitesmoke;
+        .employer-wrapper {
+            img {
+                vertical-align: middle;
+                border-radius: 1rem;
+                border: 0.1rem solid silver;
+                margin-bottom: 1rem;
+            }
+        }
+        .employer-title-wrapper {
+            display: inline-block;
+            position: relative;
+            vertical-align: middle;
+            text-align: center;
+            margin-left: 1rem;
+            h2 {
+                display: inline;
+                color: whitesmoke;
+                margin-left: 2rem;
+                &::before {
+                    position: absolute;
+                    content: '';
+                    width: 100%;
+                    height: 0.5rem;
+                    background-color: rgba(255, 255, 255, 0.1);
+                    transform: skew(45deg);
+                    left: 0;
+                    bottom: -0.2rem;
+                }
+            }
+        }
+        h3 {
+            color: $lighterGrey;
+            text-align: center;
+            width: 100%;
         }
         ol {
             list-style-type: disc;
         }
         .employer {
-            img {
-                border-radius: 1rem;
-                border: 0.1rem solid silver;
-                margin-bottom: 1rem;
+            text-align: left;
+            @media (max-width: $tabletSize) {
+                display: block;
+                text-align: center;
+            }
+            figcaption {
+                display: inline-flex;
+                vertical-align: middle;
+                margin-left: 2rem;
+                @media (max-width: $tabletSize) {
+                    vertical-align: 0;
+                    margin-left: 0;
+                }
+            }
+        }
+        .resume-block {
+            display: inline-block;
+            box-sizing: border-box;
+            width: 49.7%;
+            @media (max-width: $tabletSize) {
+                width: 100%;
             }
         }
     }
---
+++ src/theme/portfolio/rs-blurb.vue
@@ -0,0 +1,62 @@
+<template>
+    <article class="content" id="rewardstyle-section">
+        <div class="employer-wrapper columns">
+            <div class="column">
+                <img src="//res.cloudinary.com/hnairgdu6/image/upload/v1515911748/rs_ltk_byc28h.png"/>
+                <div class="employer-title-wrapper">
+                    <h2>rewardStyle + LIKEtoKNOW.it</h2>
+                </div>
+            </div>
+        </div>
+        <div class="business-content resume-block">
+            <h3>Business</h3>
+            <p>
+                More than 11,000 global influencers leverage rewardStyle’s turn-key ecosystem 
+                of simple-to-use tech products, strategic consulting, educational services 
+                and the LIKEtoKNOW.it mobile publishing and distribution platform 
+                to maximize the economic success of their content across all platforms: web, mobile and social.
+                <ol>
+                    <li> 2.3M+ LIKEtoKNOW.it users </li>
+                    <li> generating $210 million in sales to retail partners </li>
+                    <li> 1k+ original pieces of content published daily </li>
+                </ol>
+            </p>
+            <figure class="products">
+                <img src="//res.cloudinary.com/hnairgdu6/image/upload/v1515911749/rewardstyle-ltk_xaacwj.png"/>
+                <figcaption>
+                    (right to left) rewardStyle mobile app, LTK consumer app + website
+                </figcaption>
+            </figure>
+        </div>
+        <div class="technical-content resume-block">
+            <h3>Technical</h3>
+            <p>
+                Built and maintained features inside the rewardStyle iOS mobile app (influencer app and utilized technologies from the consumer app LIKEtoKNOW.it)
+                <ol>
+                    <li> Objective-C and Swift.</li>
+                    <li> CoreData for persisting favorited items within up to thousands of directories. </li>
+                    <li> MVVM + MVC + MVP architectures.</li>
+                    <li> CocoaPods for pod dependency management.</li>
+                    <li> Bitrise for continuous integration, pushing to a branch deploys a build on TestFlight.</li>
+                    <li> IGListKit for complex list views with multiple controllers.</li>
+                    <li> AFNetworking with a custom wrapper for network calls.</li>
+                    <li> Amplitude for analytics across most of the app.</li>
+                    <li> Purelayout for dynamic layouts programmatically, used interface builder for legacy views.</li>
+                    <li> Third party API integrations for sharing products to Facebook, Tumblr, Twitter, and Pinterest.</li>
+                </ol>
+                Implementation of new rewardStyle API's for mobile app consumption.
+                <ol>
+                    <li> Python and PHP for legacy endpoints.</li>
+                    <li> Stoplight for mocking up web services.</li>
+                    <li> Paw for organizing and outlining the revised LTK API.</li>
+                </ol>
+                Built part of the front end web experience for rewardStyle's Display product
+                <ol>
+                    <li> Javascript with AngularJS for the application framework. </li>
+                    <li> SASS for extending and generating CSS.</li>
+                    <li> Gulp for the build system, minifying, and linting.</li>
+                </ol>
+            </p>
+        </div>
+    </article>
+</template>
