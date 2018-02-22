7bda5c49912d680ebaa60f09de9a5600a270ecfa
Timothy Cool <tcoolpg@gmail.com>
2018-02-21 23:24:24 -0800

    animated background wip

--- src/theme/layout.vue
+++ src/theme/layout.vue
@@ -1,5 +1,6 @@
 <template>
     <div id="app">
+        <canvas id="backdrop-canvas" width=2000 height=2000 v-once></canvas>
         <message></message>
         <app-header></app-header>
         <section class="view-height">
@@ -20,6 +21,40 @@
             'app-header' : appHeader,
             'app-footer' : appFooter,
             'message' : message
+        },
+        mounted () {
+            let canvas = document.getElementById('backdrop-canvas')
+            var context = canvas.getContext('2d')
+            let box = {
+                width : 20,
+                height : 20
+            }
+            let horiz = 0
+            let i = 0
+            let vert = 0
+            let j = 0
+            context.strokeStyle = '#2f2f2f'
+            setInterval(function () {
+                context.clearRect(0, 0, 2000, 2000)
+                horiz++
+                vert++
+                let bufferX = Math.sin(Math.abs(horiz) / 100) * box.width + 6
+                let bufferY = Math.sin(Math.abs(vert) / 100) * box.height + 6
+                for (i = 0; i < 100; i++) {
+                    for (j = 0; j < 100; j++) {
+                        context.strokeRect(
+                            (box.width * i) + bufferX / 2,
+                            (box.height * j) + bufferY / 2,
+                            box.width - bufferX,
+                            box.height - bufferY
+                        )
+                    }
+                }
+                if (horiz >= 100 || vert >= 100) {
+                    horiz = -100
+                    vert = -100
+                }
+            }, 20)
         }
     }
 </script>
@@ -30,5 +65,10 @@
     @import '../styles/global.scss';
     .columns {
         flex-wrap : wrap
+    }
+    #backdrop-canvas {
+        position: fixed;
+        width: 100%;
+        z-index: 0;
     }
 </style>
