1e95fb5f9aa6b70505522687fa3c17ba4c5a59a0
Timothy Cool <tcoolpg@gmail.com>
2018-03-26 22:57:46 -0700

    fix transition from spinner to show blog posts

--- src/theme/blog.vue
+++ src/theme/blog.vue
@@ -1,20 +1,20 @@
 <template>
     <section class="blog-posts container is-fluid">
         <h1 class="content-title">Blog</h1>
-        <transition name="fade">
+        <transition name="fade" mode="out-in">
             <div class="spinner-container" v-if="showSpinner">
                 <div class="spinner">
                     <div class="loader"></div>
                 </div>
             </div>
+            <div class="list-group-item view-height" v-for="post in posts" v-bind:key="post.id">
+                <blog-post :categories='post.categories'>
+                    <h2 slot="title" class="title-text" v-html="post.title.rendered"></h2>
+                    <div class="text-wrapper" slot="content" v-html="post.content.rendered"></div>
+                </blog-post>
+                <br/>
+            </div>
         </transition>
-        <div class="list-group-item view-height" v-for="post in posts" v-bind:key="post.id">
-            <blog-post :categories='post.categories'>
-                <h2 slot="title" class="title-text" v-html="post.title.rendered"></h2>
-                <div class="text-wrapper" slot="content" v-html="post.content.rendered"></div>
-            </blog-post>
-            <br/>
-        </div>
     </section>
 </template>
 <script>
