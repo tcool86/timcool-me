a766cc61ff2bfc24d48f73850394eb99aabb9567
Timothy Cool <tcoolpg@gmail.com>
2019-11-03 21:22:07 -0800

    npm package updates (lots of garbage, need to get rid of this crap)

--- index.js
+++ index.js
@@ -1,65 +1,64 @@
-var express = require('express');
-var jsonp = require('jsonp-body');
-var app = express();
-const axios = require('axios');
-const compression = require('compression');
+var express = require('express')
+var app = express()
+const compression = require('compression')
 
-const serialize = require('serialize-javascript');
-const fs = require('fs');
-const path = require('path');
-const environment = process.env.NODE_ENV;
-const isProd = (typeof environment !== 'undefined') && (environment === 'production');
+const serialize = require('serialize-javascript')
+const fs = require('fs')
+const path = require('path')
+const environment = process.env.NODE_ENV
+const isProd = (typeof environment !== 'undefined') && (environment === 'production')
 
-const { createBundleRenderer } = require('vue-server-renderer');
-let renderer;
+const { createBundleRenderer } = require('vue-server-renderer')
+let renderer
 
-//Server app configuration
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+// Server app configuration
+app.set('port', (process.env.PORT || 5000))
+// eslint-disable-next-line no-path-concat
+app.use(express.static(__dirname + '/public'))
 
-const rootPath = (isProd) ? "/" : "/dist";
-app.use(rootPath, express.static(path.resolve(__dirname, "./dist")));
+const rootPath = (isProd) ? '/' : '/dist'
+app.use(rootPath, express.static(path.resolve(__dirname, './dist')))
 
-app.use(compression());
+app.use(compression())
 
 if (isProd) {
-    const bundlePath = path.resolve(__dirname, './dist/server/main.js');
-    renderer = createBundleRenderer(fs.readFileSync(bundlePath, 'utf-8'));
+    const bundlePath = path.resolve(__dirname, './dist/server/main.js')
+    renderer = createBundleRenderer(fs.readFileSync(bundlePath, 'utf-8'))
 } else {
-    require("./build/dev-server")(app, bundle => {
-        renderer = createBundleRenderer(bundle);
-    });
+    require('./build/dev-server')(app, bundle => {
+        renderer = createBundleRenderer(bundle)
+    })
 }
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// eslint-disable-next-line no-path-concat
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
 
 const indexHTML = (() => {
-    return fs.readFileSync(path.resolve(__dirname, "./views/pages/index.html"), "utf-8");
+    return fs.readFileSync(path.resolve(__dirname, './views/pages/index.html'), 'utf-8')
 })()
 
-//Server-side routes
+// Server-side routes
 app.get(['/', '/about', '/blog', '/portfolio', '/friend', '/stuff', '/*'], (req, res) => {
-    const context = { url: req.url }
-    console.log('context: ' + context.url);
+    const context = { url : req.url }
+    console.log('context: ' + context.url)
     renderer.renderToString(context, (error, html) => {
         if (error) {
-            console.log('error rendering: ' + error);
-            console.log('tried to load: ' + html);
-            return res.status(500).send('Server Error');
+            console.log('error rendering: ' + error)
+            console.log('tried to load: ' + html)
+            return res.status(500).send('Server Error')
         }
-        html = indexHTML.replace('{{ APP }}', html);
-        const serializedInitialState = serialize(context.initialState, { isJSON : true });
-        const stateScript = `<script>window.__INITIAL_STATE__=${serializedInitialState}</script>`;
-        html = html.replace('{{ STATE }}', stateScript);
-        res.write(html);
-        res.end();
-    });
-});
+        html = indexHTML.replace('{{ APP }}', html)
+        const serializedInitialState = serialize(context.initialState, { isJSON : true })
+        const stateScript = `<script>window.__INITIAL_STATE__=${serializedInitialState}</script>`
+        html = html.replace('{{ STATE }}', stateScript)
+        res.write(html)
+        res.end()
+    })
+})
 
-//Server listen
+// Server listen
 app.listen(app.get('port'), function () {
-    console.log('Node app is running on port', app.get('port'));
-});
-
+    console.log('Node app is running on port', app.get('port'))
+})
